3bb6ff01d5b17de96e5397407bd05802b06a89ff
KristofferHogberg <kristoffer.hogberg@hotmail.com>
2022-06-03 13:25:29 +0200

    add user UI project

---
+++ Clients/UserApp/Pages/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using UserApp.ViewModels;
+
+namespace UserApp.Pages;
+
+public class IndexModel : PageModel
+{
+    private readonly ILogger<IndexModel> _logger;
+    private readonly IConfiguration _config;
+
+    [BindProperty]
+    public List<CourseViewModel>? Courses { get; set; }
+
+    public IndexModel(ILogger<IndexModel> logger, IConfiguration config)
+    {
+        _logger = logger;
+        _config = config;
+    }
+
+    public async Task OnGetAsync()
+    {
+        var baseUrl = _config.GetValue<string>("baseUrl");
+        var url = $"{baseUrl}/courses/list";
+
+        using var http = new HttpClient();
+        Courses = await http.GetFromJsonAsync<List<CourseViewModel>>(url);
+    }
+}
---
+++ Clients/UserApp/ViewModels/CourseViewModel.cs
@@ -0,0 +1,12 @@
+namespace UserApp.ViewModels
+{
+    public class CourseViewModel
+    {
+        public int CourseId { get; set; }
+        public int CourseNo { get; set; }
+        public string? Title { get; set; }
+        public string? Length { get; set; }
+        public string? Description { get; set; }
+        public string? Details { get; set; }
+    }
+}
